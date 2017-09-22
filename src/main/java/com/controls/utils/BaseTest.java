878adb4b97920409a8b31180ed60532fa0dce536
Alex Nesov <magicofasword@gmail.com>
2017-09-22 16:50:48 +0300

    WD visibility

--- src/main/java/com/controls/utils/BaseTest.java
+++ src/main/java/com/controls/utils/BaseTest.java
@@ -30,4 +30,11 @@ public class BaseTest {
         driver.quit();
         }
 
+
+    public static void main(String[] args) {
+        BaseTest baseTest = new BaseTest();
+        baseTest.setUp();
+
+    }
+
 }
