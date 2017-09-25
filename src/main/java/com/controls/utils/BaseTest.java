2a4660247d29152830c34c8eff9bd9f74a455afe
Alex Nesov <julia@MacBook-Air-Ulenka.local>
2017-09-25 23:15:57 +0300

    wd factory used

--- src/main/java/com/controls/utils/BaseTest.java
+++ src/main/java/com/controls/utils/BaseTest.java
@@ -1,20 +1,23 @@
 package com.controls.utils;
 import java.util.concurrent.TimeUnit;
 import app.SynergyApp;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import ru.stqa.selenium.factory.LocalDriverProvider;
+import ru.stqa.selenium.factory.WebDriverPool;
 
 /**
  * Created by alexnesov on 30/03/2017.
  */
 
-public class BaseTest {
+ public abstract class BaseTest {
 
     public WebDriver driver;
-
+    
     public SynergyApp app;
 
     public BaseTest() {}
@@ -22,16 +25,14 @@ public class BaseTest {
         @BeforeTest(description="Preparing environment for the test..")
         public void setUp() {
             System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            driver = WebDriverPool.DEFAULT.getDriver(DesiredCapabilities.chrome());
+            driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
             app = PageFactory.initElements(driver,  SynergyApp.class);
-//            driver.get("https://pp.synergy.net");
          }
 
         @AfterTest (description = "Suspending....")
         public void tearDown(){
-            driver.quit();
+            WebDriverPool.DEFAULT.dismissAll();
         }
 }
 
